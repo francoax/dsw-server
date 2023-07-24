96f2b08fcdaf49417a47ccca8d2358208e547d66
francoax <francoa.duarte2001@gmail.com>
2023-07-24 19:40:21 -0300

    user and reserver models created.

---
+++ src/models/reserve.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const reserveSchema = new Schema({
+  date_start: {
+    type: Date,
+    required: true,
+  },
+  date_end: {
+    type: Date,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  package: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Package',
+    required: true,
+  },
+}, {
+  timeseries: true,
+});
+
+export default mongoose.Model('Reserve', reserveSchema);
---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    tel: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.Model('User', userSchema);

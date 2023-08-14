c5eba87392b9ee6f570cdd79a7e9151656ed5ff7
francoax <francoa.duarte2001@gmail.com>
2023-08-14 19:17:43 -0300

    routes for reserve added.

--- src/routes/reserves.routes.js
+++ src/routes/reserves.routes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import reserveController from '../controllers/reserve';
+
+import verifyMongoId from '../middlewares/mongoIdField';
 
 const router = Router();
+
+router
+  .get('/:id', [verifyMongoId], reserveController.get)
+  .get('/', reserveController.getAll)
+  .post('/', reserveController.post)
+  .put('/:id', [verifyMongoId], reserveController.put)
+  .delete('/:id', [verifyMongoId], reserveController.remove);
 
 export default router;

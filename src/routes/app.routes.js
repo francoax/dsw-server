de6ffbbaac2384ada0394eb09c508871904802cf
tomibott <tomibottoni@gmail.com>
2023-08-14 19:02:35 -0300

    I added routes file for propertie type

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,30 +1,9 @@
 import { Router } from 'express';
-import propertieTypeABM from '../controllers/propertieTypeController.js'
-
+import propertieTypeRouter from './propertieType.routes.js';
 
 const router = Router();
 
 // Imports for each route below
-router.get('/propertieType', propertieTypeABM.getAll);
-
-router.get('/propertieType/:id', propertieTypeABM.getOne);
-
-router.post('/propertieType/add', propertieTypeABM.add);
-
-router.put('/propertieType/edit/:id', propertieTypeABM.update);
-
-router.delete('/propertieType/:id', propertieTypeABM.delete);
-
-router.use((error, req, res, next)=>{
-  console.error(error);
-
-  if(error.name === 'CastError'){
-    res.status(400).send({
-      error:'category used is malformed'
-    }).end();
-  }else{
-    res.status(500).end();
-  }
-});
+router.use('/propertie-types', propertieTypeRouter);
 
 export default router;

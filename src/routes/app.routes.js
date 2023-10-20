588eedf5cbaa5dbe8d93c6996bee06cc7cd82f8a
tomibott <tomibottoni@gmail.com>
2023-10-20 19:29:00 -0300

    no anda el login en el swagger

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -20,14 +20,14 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API de mi proyecto',
+      title: 'API Documentation',
       version: '1.0.0',
-      description: 'Documentación de la API de mi proyecto',
+      description: 'Documentación de la API web de Poncho H&S',
     },
     tags: [
       {
         name: 'Users',
-        description: 'Everything about users.',
+        description: 'Documentation about user endpoints',
       },
     ],
   },
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -12,16 +12,79 @@ router.get('/me', [authenticateToken], usersController.get);
  * @openapi
  * /api/users:
  *   get:
+ *     summary: Get all Users
  *     description: Get all Users
+ *     tags:
+ *       - Users
  *     responses:
  *       200:
  *         description: Success
  *
  */
 router.get('/', usersController.getAll);
-
+/**
+ * @openapi
+ * /api/users/login:
+ *   post:
+ *     summary: Logs user into the system
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: email
+ *         required: false
+ *         schema:
+ *           type: string
+ *       - name: password
+ *         required: false
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *
+ */
 router.post('/login', usersController.login);
-
+/**
+ * @openapi
+ * /api/users:
+ *   post:
+ *     summary: Create user
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: lastname
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: address
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: email
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: password
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: tel
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: role
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *
+ */
 router.post('/', usersController.create);
 
 router.put('/', [authenticateToken], usersController.edit);

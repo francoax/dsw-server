800214ab56435fbd8981cc599c12c4730d25b828
tomibott <tomibottoni@gmail.com>
2023-10-20 18:09:09 -0300

    Cambié el parametro del cors para que me ande

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', mainRouter);

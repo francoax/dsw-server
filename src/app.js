718048e11987314242d4b2c2a70c979deb55ce9c
francoax <francoa.duarte2001@gmail.com>
2023-10-22 21:44:34 -0300

    removed unused code.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import cors from 'cors';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
 import mainRouter from './routes/app.routes.js';
 
 const app = express();
@@ -14,25 +12,6 @@ const corsOptions = {
   origin: 'https://poncho-home-and-stay-git-dev-francoax.vercel.app',
   optionsSuccessStatus: 200,
 };
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API de mi proyecto',
-      version: '1.0.0',
-      description: 'Documentación de la API de mi proyecto',
-    },
-    servers: [
-      {
-        url: 'http://localhost:9000',
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // rutas de los archivos de las rutas
-};
-
-const specs = swaggerJsdoc(options);
 
 app.use(cors(corsOptions));
 app.use(express.json());

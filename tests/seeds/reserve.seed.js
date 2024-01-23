d62bbf123b1cf051269aed34ae8d10a2a1514007
Octavio Pereyra <82680476+octapereyra@users.noreply.github.com>
2024-01-22 23:35:42 -0300

    Update reserve.seed.js

--- tests/seeds/reserve.seed.js
+++ tests/seeds/reserve.seed.js
@@ -5,14 +5,14 @@ export default [
     _id: new mongoose.Types.ObjectId('6580ce85751be545d7bebad1'),
     date_start: '2022-01-01',
     date_end: '2022-01-31',
-    user: new mongoose.Types.ObjectId('65768fce19f5c0af7df24df1'),
+    user: new mongoose.Types.ObjectId('65270324a6ecc0ccbf909cb5'),
     packageReserved: mongoose.Types.ObjectId('652dd48c82681f59fdc830a1'),
   },
   {
     _id: new mongoose.Types.ObjectId('6580ce85751be545d7bebad2'),
     date_start: '2022-01-10',
     date_end: '2022-01-01',
-    user: new mongoose.Types.ObjectId('65768fce19f5c0af7df24df2'),
+    user: new mongoose.Types.ObjectId('65270324a6ecc0ccbf909cb6'),
     packageReserved: new mongoose.Types.ObjectId('652dd48c82681f59fdc830a2'),
   },
 ];
